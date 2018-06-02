6c621ae4d2ea18bd7502f02422707d2ad696efd5
changviv <vchang103@gmail.com>
2018-06-02 22:30:47 +0000

    fix 6

--- 0x01-python_hello_world/6-print_last_letter.py
+++ 0x01-python_hello_world/6-print_last_letter.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 my_word = "Holberton"
-string = my_word[-1] + " is the last letter of the word {}"
-print(string.format(my_word)) 
+print(my_word[-1] + " is the last letter of the word {}".format(my_word)) 
